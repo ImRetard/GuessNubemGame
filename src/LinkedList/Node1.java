f6ece990ffffd52467c40da94ea09f3ccf78861a
NazarMh <nazar.myhailiv@gmail.com>
2018-09-08 14:33:21 +0300

    Add set methods

--- src/LinkedList/Node1.java
+++ src/LinkedList/Node1.java
@@ -10,4 +10,28 @@ public class Node1 {
         nextNode = null;
         previousNode = null;
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node1 getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node1 nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public Node1 getPreviousNode() {
+        return previousNode;
+    }
+
+    public void setPreviousNode(Node1 previousNode) {
+        this.previousNode = previousNode;
+    }
 }
